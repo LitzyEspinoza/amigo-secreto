c2d7a07311bafbadff9b9cc582a5eaa4c8606b9d
LitzyEspinoza <espinozalitzy4@hotmail.com>
2025-09-23 19:43:09 -0600

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []; //variable amigos
+
+function mostrarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = amigos[i];
+
+    lista.appendChild(li);
+  }
+}
